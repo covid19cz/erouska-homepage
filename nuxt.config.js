4518f84501013f409dd2f1cddf1080932aa03ec4
vitkolos <vit.kolos@gmail.com>
2020-04-15 08:48:16 +0200

    head tag + env

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,24 +1,43 @@
 
 export default {
   mode: 'universal',
+  env: {
+    description: 'Mobilní aplikace eRouška usnadní vyhledávání lidí s rizikem nákazy koronavirem v Česku v rámci systému Chytré karantény',
+    defaultLanguage: 'cs',
+    locales: ['cs', 'en']
+  },
   /*
   ** Headers of the page
   */
-  head: {
-    title: 'eRouška – chráním sebe, chráním tebe',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Mobilní aplikace eRouška usnadní vyhledávání lidí s rizikem nákazy koronavirem v Česku v rámci systému Chytré karantény' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', 'href': 'https://fonts.googleapis.com/css?family=Merriweather:400,700|Open+Sans:400,400i,700,700i&display=swap&subset=latin-ext' },
-    ],
-    script: [
-      { src: 'https://code.jquery.com/jquery-3.4.1.min.js' },
-      { src: '/main.js' }
-    ]
+  head () {
+    return {
+      title: 'eRouška – chráním sebe, chráním tebe',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: process.env.description },
+        
+        { property: 'og:type', content: 'website' },
+        { property: 'og:url', content: 'https://erouska.cz' },
+        { property: 'og:title', content: 'eRouška - Chráním sebe, chráním tebe' },
+        { property: 'og:description', content: process.env.description },
+        { property: 'og:image', content: 'https://erouska.cz/img/share-banner.png' },
+        
+        { property: 'twitter:card', content: 'summary_large_image' },
+        { property: 'twitter:url', content: 'https://erouska.cz' },
+        { property: 'twitter:title', content: 'eRouška - Chráním sebe, chráním tebe' },
+        { property: 'twitter:description', content: process.env.description },
+        { property: 'twitter:image', content: 'https://erouska.cz/img/share-banner.png' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'stylesheet', 'href': 'https://fonts.googleapis.com/css?family=Merriweather:400,700|Open+Sans:400,400i,700,700i&display=swap&subset=latin-ext' },
+      ],
+      script: [
+        { src: 'https://code.jquery.com/jquery-3.4.1.min.js' },
+        { src: '/main.js' }
+      ]
+    }
   },
   /*
   ** Customize the progress-bar color
