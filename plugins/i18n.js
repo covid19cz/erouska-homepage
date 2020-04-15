20b513971098e3b9ed33aeb223c6d35a9d59831f
vitkolos <vit.kolos@gmail.com>
2020-04-15 08:48:16 +0200

    head tag + env

--- plugins/i18n.js
+++ plugins/i18n.js
@@ -8,7 +8,7 @@ export default ({ app, store }) => {
   // This way we can use it in middleware and pages asyncData/fetch
   app.i18n = new VueI18n({
     locale: store.state.locale,
-    fallbackLocale: 'cs',
+    fallbackLocale: process.env.defaultLanguage,
     messages: require('~/locales/web.json')
   })
 
