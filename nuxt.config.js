129bcd54a4ff2a1d1749f6b879910cdbcb96bbbd
vitkolos <vit.kolos@gmail.com>
2020-04-16 15:38:55 +0200

    title tag generate fixed

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,9 +17,7 @@ export default {
     */
     head() {
         return {
-            titleTemplate: (titleChunk) => {
-                return titleChunk ? `${titleChunk} – eRouška` : 'eRouška – ' + this.$t('t.web.motto');
-            },
+            titleTemplate: '%s – eRouška',
             meta: [
                 {charset: 'utf-8'},
                 {name: 'viewport', content: 'width=device-width, initial-scale=1'},
