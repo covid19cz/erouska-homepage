10cac72223382f27fd010e859ff78a47c73ae2b5
vitkolos <vit.kolos@gmail.com>
2020-04-17 08:54:27 +0200

    t. removed

--- pages/_lang/caste-dotazy.vue
+++ pages/_lang/caste-dotazy.vue
@@ -6,17 +6,17 @@
 
         <main class="main" role="main">
             <section class="section" v-for="section in sections" :id="section.section_anchor">
-                <h2 :class="'section__title section__title--' + section.color">{{ $t('t.web.faq.sections.' + section.section_id) }}</h2>
+                <h2 :class="'section__title section__title--' + section.color">{{ $t('web.faq.sections.' + section.section_id) }}</h2>
                 <div class="section__content">
                     <div class="faq">
                         <div v-for="(question, q_index) in section.questions" :id="question.anchor"
                         :class="[q_index == 0 ? 'faq__item--open' : '', 'section__item faq__item']">
-                            <h3 class="faq__q">{{ $t('t.web.faq.questions.' + question.id + '.question') }}</h3>
+                            <h3 class="faq__q">{{ $t('web.faq.questions.' + question.id + '.question') }}</h3>
                             <div class="faq__a">
-                                <template v-for="(item, index) in Object.keys($i18n.messages[$i18n.fallbackLocale].t.web.faq.questions[question.id].answer).length">
-                                   <div v-if="['<ul>', '<ol>', '<h4>'].some(v => $t('t.web.faq.questions.' + question.id + '.answer[' + index + ']').substring(0, 4).includes(v))"
-                                   v-html="$t('t.web.faq.questions.' + question.id + '.answer[' + index + ']')"></div>
-                                   <p v-else v-html="$t('t.web.faq.questions.' + question.id + '.answer[' + index + ']')"></p>
+                                <template v-for="(item, index) in Object.keys($i18n.messages[$i18n.fallbackLocale].web.faq.questions[question.id].answer).length">
+                                   <div v-if="['<ul>', '<ol>', '<h4>'].some(v => $t('web.faq.questions.' + question.id + '.answer[' + index + ']').substring(0, 4).includes(v))"
+                                   v-html="$t('web.faq.questions.' + question.id + '.answer[' + index + ']')"></div>
+                                   <p v-else v-html="$t('web.faq.questions.' + question.id + '.answer[' + index + ']')"></p>
                                 </template>
                             </div>
                         </div>
@@ -43,7 +43,7 @@
         },
         head() {
             return {
-                title: this.$t('t.web.faq.page_title'),
+                title: this.$t('web.faq.page_title'),
                 bodyAttrs: {
                     class: 'page-single page-caste-dotazy'
                 }
