ef90d22845ac04fb6d591f855693c20f92ac8e28
Jaroslav Krch <jaroslav.krch@gmail.com>
2020-04-24 13:41:52 +0200

    overide back scroling to manual

--- app/router.scrollBehavior.js
+++ app/router.scrollBehavior.js
@@ -1,4 +1,27 @@
+
+/** overide VUE implementation for back */
+if (process.client) {
+    if ('scrollRestoration' in window.history) {
+    window.history.scrollRestoration = 'manual'
+
+    // reset scrollRestoration to auto when leaving page, allowing page reload
+    // and back-navigation from other pages to use the browser to restore the
+    // scrolling position.
+    /** original value 'auto' - that will navigate to the menu at bottom of the page */
+    window.addEventListener('beforeunload', () => {
+        window.history.scrollRestoration = 'manual'
+    })
+
+    // Setting scrollRestoration to manual again when returning to this page.
+    window.addEventListener('load', () => {
+        window.history.scrollRestoration = 'manual'
+    })
+    }
+}
+
+
 export default function (to, from, savedPosition) {
+    console.log("in" + (new Date()).getMilliseconds());
     //scroll after the call backback to avoid mixing transition and smooth scrooling
     return new Promise((resolve) => {
         setTimeout(() => {
@@ -10,3 +33,4 @@ export default function (to, from, savedPosition) {
         }, 100);
     })
 }
+
