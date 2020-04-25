3b1285d5e3d270f61bf673e5b598022d40be1589
vitkolos <vit.kolos@gmail.com>
2020-04-25 18:10:51 +0200

    changes in instructions (without web.json)

--- pages/_lang/navody.vue
+++ pages/_lang/navody.vue
@@ -1,51 +1,51 @@
 <template>
     <div>
         <section class="heading" id="uvod">
-            <h1 class="heading__title">Návody</h1>
+            <h1 class="heading__title">{{ $t('web.instructions.title') }}</h1>
         </section>  <!-- /heading -->
         <main class="main main--flex" role="main">
             <div>
                 <section class="section" id="instalace">
-                    <h2 class="section__subtitle">Instalace aplikace eRouška</h2>
+                    <h2 class="section__subtitle">{{ $t('web.instructions.installation.android.title') }}</h2>
                     <div class="section__content section__item">
-                        <p>Pro stažení eRoušky postupujte podle jednoho z následujících návodů:</p>
+                        <p>{{ $t('web.instructions.installation.android.text') }}</p>
                         <a class="link" href="/navody/instalace_aplikace_eRouska.pdf" target="_blank">
                             <img class="link__icon" src="/img/fa/file-pdf.svg" />
                             <div class="link__text">
-                                <span class="link__title">Návod na instalaci aplikace eRouška</span>
-                                <span class="link__description">PDF, 700 kB</span>
+                                <span class="link__title">{{ $t('web.instructions.installation.android.links.pdf') }}</span>
+                                <span class="link__description">{{ $t('web.instructions.descriptions.pdf_comma') + ' 700 ' + $t('web.instructions.descriptions.kilobytes') }}</span>
                             </div>
                         </a>
                         <a class="link" href="https://youtu.be/hVu71yWAIcA" target="_blank" @click.prevent="openVideo($event, 'hVu71yWAIcA')">
                             <img class="link__icon" src="/img/fa/play-circle.svg" />
                             <div class="link__text">
-                                <span class="link__title">Instalace aplikace na Android</span>
-                                <span class="link__description">Video, 2 minuty</span>
+                                <span class="link__title">{{ $t('web.instructions.installation.android.links.youtube') }}</span>
+                                <span class="link__description">{{ $t('web.instructions.descriptions.video_comma') + ' 2 ' + $t('web.instructions.descriptions.minutes') }}</span>
                             </div>
                         </a>
                     </div>
                 </section>
                 <section class="section" id="nastaveni">
-                    <h2 class="section__subtitle">Nastavení pro jednotlivé značky telefonů</h2>
+                    <h2 class="section__subtitle">{{ $t('web.instructions.settings.title') }}</h2>
                     <div class="section__content">
                         <div class="section__item">
-                            <p>Na odkazech níže najdete stručné návody pro nejpoužívanější telefony, v jejichž nastavení je potřeba zakázat automatické vypínání aplikace eRouška:</p>
+                            <p>{{ $t('web.instructions.settings.text') }}</p>
                         </div>
                         <div class="section__item" v-for="brand in instructions" :id="brand.brand_id">
-                            <h3>{{ 'web.instructions.brands.' + brand.brand_id + '.name' }}</h3>
+                            <h3>{{ $t('web.instructions.settings.brands.' + brand.brand_id + '.name') }}</h3>
                             <template v-for="link in brand.links">
-                                <a v-if="link.type === 0" class="link" :href="'/navody/' + brand.brand_folder + '/' + link.src + '.pdf'" target="_blank">
+                                <a v-if="link.type === 'pdf'" class="link" :href="'/navody/' + brand.brand_folder + '/' + link.src + '.pdf'" target="_blank">
                                     <img class="link__icon" src="/img/fa/file-pdf.svg" />
                                     <div class="link__text">
-                                        <span class="link__title">{{ 'web.instructions.brands.' + brand.brand_id + '.links.' + link.id + '.title' }}</span>
-                                        <span class="link__description">{{ 'web.instructions.brands.' + brand.brand_id + '.links.' + link.id + '.description' }}</span>
+                                        <span class="link__title">{{ $t('web.instructions.settings.brands.' + brand.brand_id + '.links.' + link.id) }}</span>
+                                        <span class="link__description">{{ $t('web.instructions.descriptions.pdf_comma') + ' ' + link.size + ' ' + $t('web.instructions.descriptions.kilobytes') }}</span>
                                     </div>
                                 </a>
-                                <a v-else class="link" :href="'https://youtu.be/' + link.src" target="_blank" @click.prevent="openVideo($event, link.src)">
+                                <a v-else-if="link.type === 'youtube'" class="link" :href="'https://youtu.be/' + link.src" target="_blank" @click.prevent="openVideo($event, link.src)">
                                     <img class="link__icon" src="/img/fa/play-circle.svg" />
                                     <div class="link__text">
-                                        <span class="link__title">{{ 'web.instructions.brands.' + brand.brand_id + '.links.' + link.id + '.title' }}</span>
-                                        <span class="link__description">{{ 'web.instructions.brands.' + brand.brand_id + '.links.' + link.id + '.description' }}</span>
+                                        <span class="link__title">{{ $t('web.instructions.settings.brands.' + brand.brand_id + '.links.' + link.id) }}</span>
+                                        <span class="link__description">{{ $t('web.instructions.descriptions.video_comma') + ' ' + link.size + ' ' + $t('web.instructions.descriptions.minutes') }}</span>
                                     </div>
                                 </a>
                             </template>
@@ -58,7 +58,7 @@
                     <h3 class="aside__title">Značky</h3>
                     <ul class="aside__actions">
                         <li v-for="brand in instructions">
-                            <a :href="'#' + brand.brand_id" class="aside__anchor"><div class="aside__anchor__title">{{ 'web.instructions.brands.' + brand.brand_id + '.name' }}</div></a>
+                            <a :href="'#' + brand.brand_id" class="aside__anchor"><div class="aside__anchor__title">{{ $t('web.instructions.settings.brands.' + brand.brand_id + '.name') }}</div></a>
                         </li>
                     </ul>
                 </div>
@@ -79,7 +79,7 @@
         },
         head() {
             return {
-                title: "Návody", //this.$t('web.faq.page_title'),
+                title: this.$t('web.instructions.page_title'),
                 bodyAttrs: {
                     class: 'page-single page-caste-dotazy page-navody'
                 }
