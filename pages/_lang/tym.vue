edcb26939363e1d4d02383717ad5a098592d530f
vitkolos <vit.kolos@gmail.com>
2020-04-25 10:59:47 +0200

    added Vít Kološ and Záchranka, fixed default photo

--- pages/_lang/tym.vue
+++ pages/_lang/tym.vue
@@ -15,9 +15,8 @@
                         <ul class="team__list">
                             <li v-for="person in section.people">
                                 <a :title="$t('web.team.member_link_title')" :href="person.linkedin" target="_blank">
-                                    <img :src="person.photoUrl" class="team__list__photo"
-                                         :alt="person.name + ' ' + person.surname"/>{{person.name}}
-                                    {{person.surname}}
+                                    <img :src="person.photoUrl ? person.photoUrl : 'https://erouska.cz/img/photo-avatar.png'" class="team__list__photo" />
+                                    {{person.name}} {{person.surname}}
                                 </a>
                             </li>
                         </ul>
@@ -40,30 +39,20 @@
             <section class="section section--full">
                 <div class="section__content">
                     <ul class="partners">
-                        <li><a href="https://www.avast.com/cs-cz/index#pc" target="_blank"><img
-                            src="/img/partners/logo-avast.png"/></a></li>
-                        <li><a href="https://www.ysoft.com/" target="_blank"><img
-                            src="/img/partners/logo-ysoft.png"/></a></li>
-                        <li><a href="https://antelli.io" target="_blank"><img
-                            src="/img/partners/logo-antelli.png"/></a></li>
-                        <li><a href="https://60seconds.cz/" target="_blank"><img
-                            src="/img/partners/logo-60seconds.png"/></a></li>
-                        <li><a href="https://www.teststack.cz/" target="_blank"><img
-                            src="/img/partners/logo-teststack.png"/></a></li>
-                        <li><a href="http://expertkom.cz/" target="_blank"><img
-                            src="/img/partners/logo-expertkom.png"/></a></li>
-                        <li><a href="https://www.clevermaps.io/" target="_blank"><img
-                            src="/img/partners/logo-clevermaps.png"/></a></li>
-                        <li><a href="https://settleup.io" target="_blank"><img
-                            src="/img/partners/logo-settleup.png"/></a></li>
-                        <li><a href="https://adastra.one" target="_blank"><img
-                            src="/img/partners/logo-adastra.png"/></a></li>
-                        <li><a href="https://www.novalia.cz/#home" target="_blank"><img
-                            src="/img/partners/logo-novalia.png"/></a></li>
-                        <li><a href="https://www.mobilnirozhlas.cz/" target="_blank"><img
-                            src="/img/partners/logo-mobilnirozhlas.png"/></a></li>
-                        <li><a href="https://svejda-goldmann.cz/" target="_blank"><img
-                            src="/img/partners/logo-svejda-goldmann.png"/></a></li>
+                        <li><a href="https://www.avast.com/cs-cz/index#pc" target="_blank"><img src="/img/partners/logo-avast.png"/></a></li>
+                        <li><a href="https://www.ysoft.com/" target="_blank"><img src="/img/partners/logo-ysoft.png"/></a></li>
+                        <li><a href="https://antelli.io" target="_blank"><img src="/img/partners/logo-antelli.png"/></a></li>
+                        <li><a href="https://60seconds.cz/" target="_blank"><img src="/img/partners/logo-60seconds.png"/></a></li>
+                        <li><a href="https://www.teststack.cz/" target="_blank"><img src="/img/partners/logo-teststack.png"/></a></li>
+                        <li><a href="http://expertkom.cz/" target="_blank"><img src="/img/partners/logo-expertkom.png"/></a></li>
+                        <li><a href="https://www.clevermaps.io/" target="_blank"><img src="/img/partners/logo-clevermaps.png"/></a></li>
+                        <li><a href="https://settleup.io" target="_blank"><img src="/img/partners/logo-settleup.png"/></a></li>
+                        <li><a href="https://adastra.one" target="_blank"><img src="/img/partners/logo-adastra.png"/></a></li>
+                        <li><a href="https://www.novalia.cz/#home" target="_blank"><img src="/img/partners/logo-novalia.png"/></a></li>
+                        <li><a href="https://www.mobilnirozhlas.cz/" target="_blank"><img src="/img/partners/logo-mobilnirozhlas.png"/></a></li>
+                        <li><a href="https://svejda-goldmann.cz/" target="_blank"><img src="/img/partners/logo-svejda-goldmann.png"/></a></li>
+                        <li><a href="https://svejda-goldmann.cz/" target="_blank"><img src="/img/partners/logo-svejda-goldmann.png"/></a></li>
+                        <li><a href="https://www.zachrankaapp.cz/" target="_blank"><img src="/img/partners/logo-zachranka.png"/></a></li>
                     </ul>
                 </div>
             </section>
