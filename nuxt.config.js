4ca99dda95a8f8de190d8bc89b808b6a916d43f6
vitkolos <vit.kolos@gmail.com>
2020-05-02 16:25:50 +0200

    generate press page

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,5 @@
 const locales = ['cs', 'en'];
-const pages = ['', 'audit-kod', 'caste-dotazy', 'gdpr', 'navody', 'tym'];
+const pages = ['', 'audit-kod', 'caste-dotazy', 'gdpr', 'napsali-o-nas', 'navody', 'tym'];
 const routes = [];
 for (const locale of locales) {
     for (const page of pages) {
