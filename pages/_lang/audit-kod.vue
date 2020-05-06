e3aea7ed5cdc6e07200d0af92a128cf31f1d3f25
vitkolos <vit.kolos@gmail.com>
2020-05-06 08:49:31 +0200

    added cvut audit

--- pages/_lang/audit-kod.vue
+++ pages/_lang/audit-kod.vue
@@ -10,11 +10,12 @@
                         <p>{{ $t('web.audit.section1.perex') }} <nuxt-link :to="homeUrl + 'gdpr'">{{ $t('web.audit.section1.perex_gdpr') }}</nuxt-link>.</p>
                         <ul>
                             <li><a href="/downloads/cvut.pdf" target="_blank">{{ $t('web.audit.section1.review_cvut_1') }}</a></li>
-                            <li><a href="/downloads/CVUT_2.pdf" target="_blank">{{ $t('web.audit.section1.review_cvut_2') }}</a></li>
+                            <li><a href="/downloads/cvut2.pdf" target="_blank">{{ $t('web.audit.section1.review_cvut_2') }}</a></li>
                             <li><a href="https://www.linkedin.com/pulse/aplikace-erou%C5%A1ka-m%C3%A1-moji-d%C5%AFv%C4%9Bru-ond%C5%99ej-vesel%C3%BD"
                                    target="_blank">{{ $t('web.audit.section1.review_vesely') }}</a></li>
                             <li><a href="https://idea.cerge-ei.cz/vystupy/vyuziti-technologie-bluetooth-pro-trasovani-sireni-covid-19"
                                    target="_blank">{{ $t('web.audit.section1.review_cerge') }}</a></li>
+                            <li><a href="/downloads/cvut3.pdf" target="_blank">{{ $t('web.audit.section1.review_cvut_3') }}</a></li>
                         </ul>
                         <p>{{ $t('web.audit.section1.code_text') }} (<a href="https://github.com/covid19cz/erouska-android"
                         target="_blank">Android</a>, <a href="https://github.com/covid19cz/erouska-ios" target="_blank">iOS</a>).</p>
