9038c412d26d97fc9af7ef8a5ec0a1b8166e249b
vitkolos <vit.kolos@gmail.com>
2020-06-24 22:59:09 +0200

    locales temporarily removed

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,4 @@
-const locales = ['cs', 'en', 'vi', 'ro', 'ru'];
+const locales = ['cs', 'en'];
 const localeCaptions = {cs: 'Čeština', en: 'English', vi: 'Tiếng Việt', ro: 'Română', ru: 'Русский', uk: 'Українська', pt: 'Português', nl: 'Nederlands', sk: 'Slovenčina'};
 const pages = ['', 'audit-kod', 'caste-dotazy', 'cookies', 'gdpr', 'napsali-o-nas', 'navody', 'tym'];
 const routes = [];
