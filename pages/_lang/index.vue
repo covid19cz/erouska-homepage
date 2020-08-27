5ce7352435f6a82b37b609619fbe39027b0c39f7
vitkolos <vit.kolos@gmail.com>
2020-08-27 11:50:01 +0200

    new index page

--- pages/_lang/index.vue
+++ pages/_lang/index.vue
@@ -5,6 +5,7 @@
                 <h2 class="hero__title"><span>{{ $t('web.index.title.part_1') }}</span> <br>{{ $t('web.index.title.part_2') }}</h2>
                 <div class="hero__intro">
                     <p v-html="$t('web.index.intro.part_1')"></p>
+                    <p>{{ $t('web.index.intro.part_1_extended') }}</p>
                     <p>{{ $t('web.index.intro.part_2') }}</p>
                 </div>
                 <ul class="hero__actions">
@@ -25,18 +26,17 @@
         <main class="main d-flex" role="main">
             <div>
                 <section class="section section--first">
-                    <h2 class="section__title section__title--blue" id="jak-erouska-chrani">{{ $t('web.index.new_app') }}</h2>
+                    <h2 class="section__title section__title--blue" id="jak-erouska-chrani">{{ $t('web.index.key_message') }}</h2>
+                    <h3 class="section__subtitle">{{ $t('web.index.smart_quarantine') }}</h3>
                     <div class="section__content">
                         <div class="section__item">
-                            <i18n path="web.index.basic_version_info" tag="p">
-                                <a href="https://covid19cz.cz/" target="_blank">Covid19CZ</a>
-                                <a href="https://www.mzcr.cz/" target="_blank">{{ $t('web.index.who_is_running_it.answer.ministry') }}</a>
-                                <a href="https://nakit.cz/" target="_blank">{{ $t('web.index.who_is_running_it.answer.nakit') }}</a>
-                            </i18n>
+                            <h3>{{ $t('web.index.you_have_meet_infected.question') }}</h3>
+                            <p>{{ $t('web.index.you_have_meet_infected.answer') }}</p>
                         </div>
                         <div class="section__item">
-                            <h3>{{ $t('web.index.functions_question') }}</h3>
-                            <ul v-html="$t('web.index.functions_list')"></ul>
+                            <h3>{{ $t('web.index.when_you_become_infected.question') }}</h3>
+                            <p>{{ $t('web.index.when_you_become_infected.answer_1') }}</p>
+                            <p><em>{{ $t('web.index.when_you_become_infected.answer_2') }}</em></p>
                         </div>
                     </div>
                 </section>
@@ -48,17 +48,9 @@
                             <p>{{ $t('web.index.how_does_erouska_differ.answer') }}</p>
                         </div>
                         <div class="section__item">
-                            <h3>{{ $t('web.index.basic_version_comparison.question') }}</h3>
-                            <p>{{ $t('web.index.basic_version_comparison.answer') }}</p>
-                        </div>
-                        <div class="section__item">
                             <h3>{{ $t('web.index.me_identity.question') }}</h3>
-                            <p>{{ $t('web.index.me_identity.answer') }}</p>
-                        </div>
-                        <div class="section__item">
-                            <h3>{{ $t('web.index.data_security.question') }}</h3>
-                            <i18n path="web.index.data_security.answer.text" tag="p">
-                                <nuxt-link :to="homeUrl + 'audit-kod'">{{ $t('web.index.data_security.answer.independent_audits') }}</nuxt-link>
+                            <i18n path="web.index.me_identity.answer" tag="p">
+                                <nuxt-link :to="homeUrl + 'gdpr'">{{ $t('web.index.me_identity.link') }}</nuxt-link>
                             </i18n>
                         </div>
                         <div class="section__item">
@@ -68,12 +60,26 @@
                         <div class="section__item">
                             <h3>{{ $t('web.index.who_is_running_it.question') }}</h3>
                             <i18n path="web.index.who_is_running_it.answer.text" tag="p">
-                                <a href="https://covid19cz.cz/" target="_blank">Covid19CZ</a>
+                                <nuxt-link :to="homeUrl + 'audit-kod'">{{ $t('web.index.who_is_running_it.answer.independent_audits') }}</nuxt-link>
                                 <a href="https://www.mzcr.cz/" target="_blank">{{ $t('web.index.who_is_running_it.answer.ministry') }}</a>
                                 <a href="https://nakit.cz/" target="_blank">{{ $t('web.index.who_is_running_it.answer.nakit') }}</a>
-                                <nuxt-link :to="homeUrl + 'tym'">{{ $t('web.index.who_is_running_it.answer.dev_team') }}</nuxt-link>
                             </i18n>
                         </div>
+                        <div class="section__item">
+                            <h3>{{ $t('web.index.who_made_it.question') }}</h3>
+                            <i18n path="web.index.who_made_it.answer.text" tag="p">
+                                <nuxt-link :to="homeUrl + 'tym'">{{ $t('web.index.who_made_it.answer.dev_team') }}</nuxt-link>
+                            </i18n>
+                        </div>
+                        <div class="section__item">
+                            <h3>{{ $t('web.index.further_questions.question') }}</h3>
+                            <p>{{ $t('web.index.further_questions.more') }}</p>
+                        </div>
+                        <ul class="section__actions">
+                            <li>
+                                <nuxt-link :to="homeUrl + 'caste-dotazy'" class="button button--red">{{ $t('web.index.further_questions.goto') }}</nuxt-link>
+                            </li>
+                        </ul>
                     </div>
                 </section>
             </div>
