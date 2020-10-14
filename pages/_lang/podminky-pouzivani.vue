d87a5fde41db9b51b9e877638cbfbabf2a305536
vitkolos <vit.kolos@gmail.com>
2020-10-14 10:10:32 +0200

    removed problematic regex (safari hopefully fixed)

--- pages/_lang/podminky-pouzivani.vue
+++ pages/_lang/podminky-pouzivani.vue
@@ -121,7 +121,8 @@
 
         methods: {
             insertNbsp(text) {
-                return this.$i18n.locale != this.$i18n.fallbackLocale ? text : text.replace(/(?<=\s)([kvszaiou])\s/gi, '$1\u00A0');
+                // return this.$i18n.locale != this.$i18n.fallbackLocale ? text : text.replace(/(?<=\s)([kvszaiou])\s/gi, '$1\u00A0');
+                return text;
             },
 
             // active navigation from https://css-tricks.com/sticky-smooth-active-nav/
